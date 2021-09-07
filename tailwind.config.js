8a25e3e6ac029bb2a9172815959f5d0dec0c6b84
Amanda Romero <mandicap@comcast.net>
2021-09-07 10:39:15 -0600

    Unless curating colors, custom colors should the theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,11 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
   purge: ['./source/**/*.blade.php'],
   theme: {
-    colors: {
-      primary: '#0A47E2',
-      secondary: '#FCD925',
-    },
     extend: {
+      colors: {
+        primary: '#0A47E2',
+        secondary: '#FCD925',
+      },
       fontFamily: {
         sans: ['Inter', ...defaultTheme.fontFamily.sans],
         heading: ['Work Sans', defaultTheme.fontFamily.sans]
