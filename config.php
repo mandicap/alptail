f578d167603ebb4cd4703efec09ef01f914707af
Amanda Romero <mandicap@comcast.net>
2021-09-07 21:15:12 -0600

    Removes trailing slash from baseUrl

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 
 return [
     'production' => false,
-    'baseUrl' => 'http://locahost:3000/',
+    'baseUrl' => 'http://locahost:3000',
     'title' => 'Alptail',
     'description' => 'A collection of open-source UI components, using Tailwind.css and Alpine.js. Regularly updated and on a mission to make them as accessible as possible. Maintained by Daniel Palmer.',
     'collections' => [],
--- config.production.php
+++ config.production.php
@@ -2,7 +2,7 @@
 
 return [
     'production' => true,
-    'baseUrl' => 'https://www.alptail.com/',
+    'baseUrl' => 'https://www.alptail.com',
     'title' => 'Alptail',
     'description' => 'A collection of open-source UI components, using Tailwind.css and Alpine.js. Regularly updated and on a mission to make them as accessible as possible. Maintained by Daniel Palmer.',
 ];
